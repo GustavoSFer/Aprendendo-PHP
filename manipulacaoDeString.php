ad06b5c11ef35593c2c37244be9dd412b5395a5f
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-12-15 17:23:22 -0300

    Explicando sobre manipulação de string

---
+++ manipulacaoDeString.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Manipulação de Strings</title>
+</head>
+<body>
+  <?php
+  // strtolower($texto) => Transforma todos os caracteres da string em minusculos;
+  // strtoupper($texto) => Transforma todos os caracteres da string em maiusculos;
+  // ucfirst($texto) => Transforma o primeiro caracter da string em maiusculo;
+  // strlen($texto) => Conta a quantidade de caracteres de uma string;
+  // str_replace(<procura por>, <substitui por>, $texto) =>  Subistitui uma cadeia de caracteres por outra dentro de uma string;
+  // substr($texto, <posicao inicial> , <qtde caracteres>) => Retorna parte de uma string;
+
+
+
+
+
+    $texto = ''
+  
+  ?>
+</body>
+</html>
