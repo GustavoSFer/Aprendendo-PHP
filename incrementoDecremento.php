3fe1878d6cdcafc16ab8b2eb9479d682228f02a4
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-12-15 15:16:41 -0300

    Explicando

---
+++ incrementoDecremento.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Incremento e Decremento</title>
+</head>
+<body>
+  <?php
+  
+    // ++$a => Adiciona uma unidade antes de retornar $a
+    // $a++ => Retorna $a e depois adicona uma unidade
+    // --$a => Diminui uma unidade antes de retornar $a
+    // $a-- => Retorna $a e depois diminui uma unidade
+
+  ?>
+</body>
+</html>
