eb8afd34970b943217022eb04a2fb46c0eb64555
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-12-19 14:33:44 -0300

    Fazendo o calculo entre os dias

--- datas.php
+++ datas.php
@@ -12,6 +12,35 @@
 
     echo date('d/m/Y H:i'); //Verificar os token de tipo no site da PHP
 
+    echo date_default_timezone_get(); // => Retorna o timezone defaul da aplicação.
+    echo '<br />';
+
+    echo date_default_timezone_set('America/Sao_Paulo'); // => Atualiza o timezone default da aplicação.
+    // Timezone - verificar no site PHP.
+    echo '<br />';
+
+
+    // Espero sempre no parametro americano. (ano/mes/dia);
+    $data_inicial = '2018-04-24';
+    $data_final = '2018-05-15'; 
+
+    $time_inicial = strtotime($data_inicial); // => Transforma datas textuais em segundos.
+    $time_final = strtotime($data_final); // => Transforma datas textuais em segundos.
+
+    $diferenca_times = $time_final - $time_inicial;
+
+    echo '<br />';
+    echo 'A diferença de segundos entre as data é ' . $diferenca_times;
+
+    $segungos_existe_dia = 24 * 60 * 60;
+    echo '<br />';
+    echo 'Um dia possui ', $segungos_existe_dia . ' segundos';
+
+    $diferenca_de_dias_entre_datas = $diferenca_times / $segungos_existe_dia;
+
+    echo 'A diferença de dias é '. $diferenca_de_dias_entre_datas;
+    
+
   ?>
 
 </body>
