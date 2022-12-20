84b362b243a5fe128954cc4774ae54b88d0fac63
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-12-20 18:07:16 -0300

    manipulando arrays

---
+++ manipulacaoDeArrays.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Manipulação de Arrays</title>
+</head>
+<body>
+  
+  <?php
+
+  /*
+    is_array(array) -> Verifica se o paramentro é um array;
+    array_keys(array) -> Retorna todas as chaves de um array;
+    sort(array) -> Ordena um array e reajusta seus indices;
+    asort(array) -> Ordena um array preservando os indices;
+    count(array) -> Conta a quantidade de elementos de um array;
+    array_merge(array) -> Funde um ou mais arrays;
+    explode(array) -> Divide uma string baseada em um delimitador;
+    implode(array) -> Junta elementos de um array em uma string;
+  */
+
+  
+  ?>
+
+</body>
+</html>
